b0a5c07c9fd28ff866d7738f7b28d3865e0ea7f2
Evgeniy Palguev <dizverst@gmail.com>
2017-09-28 02:32:07 +0300

    Initial commit. Create Dowload Data class

---
+++ app/src/main/java/com/example/android/top10downloader/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.android.top10downloader;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    private class DownloadData extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }
+    }
+}
