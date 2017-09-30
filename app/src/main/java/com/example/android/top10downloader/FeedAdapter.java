bee6fbb67c0a25c8d6618c7aa7d51a007f1b2f70
digitalfinesse <dizverst@gmail.com>
2017-10-01 02:06:04 +0300

    Add Feed Adapter

---
+++ app/src/main/java/com/example/android/top10downloader/FeedAdapter.java
@@ -0,0 +1,23 @@
+package com.example.android.top10downloader;
+
+import android.content.Context;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.widget.ArrayAdapter;
+
+import java.util.List;
+
+public class FeedAdapter extends ArrayAdapter {
+    private static final String TAG = "FeedAdapter";
+    private final int layoutResource;
+    private final LayoutInflater layoutInflater;
+    private final List<FeedEntry> applications;
+
+    public FeedAdapter(@NonNull Context context, @LayoutRes int resource, List<FeedEntry> applications) {
+        super(context, resource);
+        this.layoutResource = resource;
+        this.layoutInflater = LayoutInflater.from(context);
+        this.applications = applications;
+    }
+}
